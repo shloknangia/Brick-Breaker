12a4fa3cdb428f0d3b5fd4a80bd07c5c0ab391ea
shlok nangia <shloknangia@gmail.com>
2019-01-13 11:58:12 +0530

    html and js files added

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById('gameScreen');
+let ctx = canvas.getContext('2d');//rendering context
+
+
+ctx.fillStyle = '#f00'; 
+ctx.fillRect(20, 20, 20, 10);
+ctx.fillRect(100, 100, 50, 50);
