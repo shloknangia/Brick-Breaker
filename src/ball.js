e997bfed585cd58d982765b4a7e057b7301a00cc
shlok nangia <shloknangia@gmail.com>
2019-01-13 15:55:05 +0530

    colllision between ball and paddle

--- src/ball.js
+++ src/ball.js
@@ -15,6 +15,7 @@ class Ball {
         };
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
+        this.game = game;
     }
 
     draw(ctx){
@@ -22,6 +23,8 @@ class Ball {
     }
 
     update(deltaTime){
+        // console.log(this.game.paddle.position.x);
+        
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;        
 
@@ -30,6 +33,18 @@ class Ball {
         }
         if(this.position.y > this.gameHeight - this.size.height || this.position.y <0){
             this.speed.y = -this.speed.y;
+        }
+
+        let bottomOfBall = this.position.y + this.size.height;
+        let topOfPaddle = this.game.paddle.position.y;
+        let leftOfPaddle = this.game.paddle.position.x;
+        let rightOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+        if(bottomOfBall >= topOfPaddle 
+            && this.position.x >= leftOfPaddle
+            && this.position.x + this.size.width < rightOfPaddle
+            ){
+            this.speed.y = -this.speed.y;
+            this.position.y = this.game.paddle.position.y - this.size.height;
         }
     }
 }
--- src/game.js
+++ src/game.js
@@ -5,21 +5,27 @@ class Game {
         this.gameHeight = gameHeight;
         this.gameWidth = gameWidth;
         
+        
     }
 
     start(){
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
+        this.gameObjects = [this.ball, this.paddle];
         new InputHandler(this.paddle);
     }
 
     update(deltatime){
-        this.paddle.update(deltatime);
-        this.ball.update(deltatime);
+        // this.paddle.update(deltatime);
+        // this.ball.update(deltatime);
+
+        this.gameObjects.forEach(obj => obj.update(deltatime));
     }
 
     draw(ctx){
-        this.paddle.draw(ctx);
-        this.ball.draw(ctx);
+        // this.paddle.draw(ctx);
+        // this.ball.draw(ctx);
+
+        this.gameObjects.forEach(obj => obj.draw(ctx));
     }
 }
