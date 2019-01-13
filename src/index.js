6864f441c10abacd6ec50051ace2a980cb1ee05c
shlok nangia <shloknangia@gmail.com>
2019-01-13 12:23:26 +0530

    paddle.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import Paddle from './paddle.js';
 let canvas = document.getElementById('gameScreen');
-let ctx = canvas.getContext('2d');//rendering context
+let ctx = canvas.getContext("2d");//rendering context
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
 
 
-ctx.fillStyle = '#f00'; 
+// ctx.clearRect(0,0,800,800);
+// ctx.fillStyle = '#f00'; 
 ctx.fillRect(20, 20, 20, 10);
-ctx.fillRect(100, 100, 50, 50);
+// ctx.fillRect(100, 100, 50, 50);
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+// paddle.draw(ctx); 
+
