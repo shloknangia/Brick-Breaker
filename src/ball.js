35430c622fefa7f13a0989bda296ade02acb87b0
shlok nangia <shloknangia@gmail.com>
2019-01-13 17:22:09 +0530

    collision detection between ball and brick

--- src/ball.js
+++ src/ball.js
@@ -9,17 +9,14 @@ class Ball {
             x: 10,
             y: 10,
         };
-        this.size = {
-            width: 16,
-            height: 16,
-        };
+        this.size = 16;
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
         this.game = game;
     }
 
     draw(ctx){
-        ctx.drawImage(this.image, this.position.x, this.position.y, this.size.width, this.size.height);
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.size, this.size);
     }
 
     update(deltaTime){
@@ -28,23 +25,16 @@ class Ball {
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;        
 
-        if(this.position.x > this.gameWidth - this.size.width || this.position.x < 0 ){
+        if(this.position.x > this.gameWidth - this.size || this.position.x < 0 ){
             this.speed.x = -this.speed.x;
         }
-        if(this.position.y > this.gameHeight - this.size.height || this.position.y <0){
+        if(this.position.y > this.gameHeight - this.size || this.position.y <0){
             this.speed.y = -this.speed.y;
         }
 
-        let bottomOfBall = this.position.y + this.size.height;
-        let topOfPaddle = this.game.paddle.position.y;
-        let leftOfPaddle = this.game.paddle.position.x;
-        let rightOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
-        if(bottomOfBall >= topOfPaddle 
-            && this.position.x >= leftOfPaddle
-            && this.position.x + this.size.width < rightOfPaddle
-            ){
+        if(detectCollision(this, this.game.paddle)){
             this.speed.y = -this.speed.y;
-            this.position.y = this.game.paddle.position.y - this.size.height;
+            this.position.y = this.game.paddle.position.y - this.size;
         }
     }
 }
--- src/brick.js
+++ src/brick.js
@@ -1,20 +1,32 @@
 class Brick {
-    constructor(game, position){
-        this.image = document.getElementById('img_brick');
-        this.position = position;
-        this.size = {
-            width: 80,
-            height: 24,
-        };
-        
-        this.game = game;
+    constructor(game, position) {
+      this.image = document.getElementById("img_brick");
+  
+      this.game = game;
+  
+      this.position = position;
+      this.width = 80;
+      this.height = 24;
+  
+      this.markedForDeletion = false;
     }
-
-    draw(ctx){
-        ctx.drawImage(this.image, this.position.x, this.position.y, this.size.width, this.size.height);
+  
+    update() {
+      if (detectCollision(this.game.ball, this)) {
+        this.game.ball.speed.y = -this.game.ball.speed.y;
+  
+        this.markedForDeletion = true;
+      }
     }
-
-    update(){
-
+  
+    draw(ctx) {
+      ctx.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+      );
     }
-}
+  }
+  
