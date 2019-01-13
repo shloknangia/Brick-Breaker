2fe9b72bf0f41d848fe833abc6cc241dbc2fd8d2
shlok nangia <shloknangia@gmail.com>
2019-01-13 13:06:00 +0530

    paddle made

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Paddle from './paddle.js';
+'use strict';
 let canvas = document.getElementById('gameScreen');
 let ctx = canvas.getContext("2d");//rendering context
 const GAME_WIDTH = 800;
@@ -6,10 +6,10 @@ const GAME_HEIGHT = 600;
 
 
 // ctx.clearRect(0,0,800,800);
-// ctx.fillStyle = '#f00'; 
-ctx.fillRect(20, 20, 20, 10);
+ctx.fillStyle = '#f00'; 
+// ctx.fillRect(20, 20, 20, 10);
 // ctx.fillRect(100, 100, 50, 50);
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-// paddle.draw(ctx); 
+paddle.draw(ctx); 
 
--- src/paddle.js
+++ src/paddle.js
@@ -1,14 +1,17 @@
-export class Paddle {
+class Paddle {
     constructor(gameWidth, gameHeight){
         this.width = 150;
         this.height = 30;
         this.position = {
-            x: gameWidth/2 - this.width/2,
-            y: gameHeight - this.height - 10,
+            x: (gameWidth/2 - this.width/2),
+            y: (gameHeight - this.height - 10),
         }
     }
 
     draw(ctx) {
-        ctx.fillRect(0,0, 10, 10);
+        ctx.fillStyle = '#0ff';
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
+
+// module.exports = Paddle;
